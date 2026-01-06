d26df55a21ebf9a9b3e6e40c77e80ba8cba4cd9c
Dharmik Patel <dharmik.rising@gmail.com>
2026-01-06 16:32:52 +0530

    start challage ui done

---
+++ Alarmify/MVVM/Controller/AddNewAlarm/SubVC/StartChallenges/View/StartChallengesVC.swift
@@ -0,0 +1,27 @@
+//
+//  StartChallengesVC.swift
+//  Alarmify
+//
+//  Created by Ankit on 06/01/26.
+//
+
+import UIKit
+
+class StartChallengesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedWakeUp(_ sender: Any) {
+    }
+    @IBAction func tappedStartChallege(_ sender: Any) {
+    }
+    @IBAction func tappedSnoze(_ sender: Any) {
+    }
+    
+     
+
+}
